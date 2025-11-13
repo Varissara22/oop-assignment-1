a1c1d77f4e351b48a557a6b3986307cf05103400
Varissara <varissara.apong@gmail.com>
2025-11-13 16:37:20 +0700

    Third commit when the second class is developed and tested

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -5,3 +5,10 @@ class Book:
         self.author = author
         self.total_copies = total_copies
         self.available_copies = total_copies
+
+class Member:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_books_list = []
