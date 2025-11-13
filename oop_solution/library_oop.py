b35934c41fcf2a2362f47acf0149dbb27bc6efb2
Varissara <varissara.apong@gmail.com>
2025-11-13 16:36:10 +0700

    Second commit when one class is developed and tested

---
+++ oop_solution/library_oop.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self,  id, title, author, total_copies):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.total_copies = total_copies
+        self.available_copies = total_copies
